fb7492f8e3bcecb0498bdd63518368353a4d1979
TheEpicBlock <git.teb@theepicblock.nl>
2022-05-13 22:28:56 +0200

    Fix checkstyle in BannerBlockEntityClientMixin

--- src/main/java/io/github/fablabsmc/fablabs/mixin/bannerpattern/client/BannerBlockEntityClientMixin.java
+++ src/main/java/io/github/fablabsmc/fablabs/mixin/bannerpattern/client/BannerBlockEntityClientMixin.java
@@ -7,7 +7,6 @@ import com.mojang.datafixers.util.Pair;
 import io.github.fablabsmc.fablabs.impl.bannerpattern.LoomPatternConversions;
 import io.github.fablabsmc.fablabs.impl.bannerpattern.LoomPatternData;
 import io.github.fablabsmc.fablabs.impl.bannerpattern.iface.LoomPatternContainer;
-import net.minecraft.block.entity.BannerPattern;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
@@ -16,6 +15,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import net.minecraft.block.entity.BannerPattern;
 import net.minecraft.block.entity.BannerBlockEntity;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.item.ItemStack;
